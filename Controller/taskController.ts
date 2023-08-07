041ea4a99c78341784d2ac29bfe89b6c8bb70b4c
umeh emmanuel <eumeh3882@gmail.com>
2023-08-07 08:50:46 -0700

    emmanuel

---
+++ Controller/taskController.ts
@@ -0,0 +1,9 @@
+import express, {Request, Response} from "express"
+
+export  const createTask =async(req:Request, res:Response)=>{
+    try {
+        
+    } catch (error) {
+        return res.status(404).json
+    }
+}
